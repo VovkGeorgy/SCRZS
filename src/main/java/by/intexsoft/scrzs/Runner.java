9e4d2a7656ef83531f20e88314333761934e3025
Heorhi Vovk <heorhi.vovk@INTEXSOFT.BY>
2019-02-18 17:22:05 +0300

    Rabbitmq receiving messages work

--- src/main/java/by/intexsoft/scrzs/Runner.java
+++ src/main/java/by/intexsoft/scrzs/Runner.java
@@ -1,31 +1,19 @@
 package by.intexsoft.scrzs;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import by.intexsoft.scrzs.service.RabbitConnectionService;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class Runner {
 
-    public static void main(String[] args) throws IOException, TimeoutException {
+    public static void main(String[] args) {
 //        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class, RabbitConfig.class);
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setUsername("guest");
-        factory.setPassword("guest");
-        factory.setVirtualHost("/");
-        factory.setHost("localhost");
-        factory.setPort(5673);
-        Connection conn = factory.newConnection();
-        Channel channel = conn.createChannel();
-        channel.exchangeDeclare("AMQP default", "direct", true);
-        String queueName = channel.queueDeclare().getQueue();
-        channel.queueBind(queueName, "AMQP default", "black");
-
-        byte[] messageBodyBytes = "New hello world".getBytes();
-        channel.basicPublish("AMQP default", "black", null, messageBodyBytes);
-
+        try {
+            RabbitConnectionService.testConnection();
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
         System.out.println("Running");
     }
 }
